29870edfa48642b104598060c975fc8fe8c8ef8f
weskley <weskley@PC-WESKLEY>
2016-06-10 19:49:56 -0300

    Primeiro Commit

---
+++ src/Lista3/Questao10.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista3;
+
+/**
+ *
+ * @author weskley
+ */
+public class Questao10 {
+    
+}
